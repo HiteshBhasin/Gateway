3bce5f5fc478b0a4d43165ae63083e7b3b267e74
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-03-16 17:14:15 +0000

    new commit with HWT

--- gateway.js
+++ gateway.js
@@ -2,23 +2,39 @@ const express = require("express");
 const app = express();
 const {createProxyMiddleware} = require("http-proxy-middleware");
 require('dotenv').config();
-
+//https://www.geeksforgeeks.org/json-web-token-jwt/
+const jwt = require("jasonwebtoken");
 const port = process.env.PORT || 3000;
+const key = process.env.SECRET_KEY || "superkey";
 
-
-
+// below two wont work because we do not have set up 
+//the two servers and run them , once we have set up the server 
+//the proxy will redirect to the respected servers. 
 const SERVICE = {
-    users: 'https//localhost:3001',
-    orders: 'https//localhost:3002' 
+    users: 'http://localhost:3001/users',
+
+    orders: 'http://localhost:3002/orders' 
 };
 
 app.get("/status", (req,res)=>{
     res.send("running a http proxy api")
 });
-
+// learning "createProxyMiddleware", https://dzone.com/articles/how-to-build-a-nodejs-api-proxy-using-http-proxy-m
 const proxyWorker = createProxyMiddleware({
     target: SERVICE.users,
     changeOrigin: true,
-})
+    pathRewrite: { '^/api/users': '/users' }, 
+});
+const proxyWorker2 = createProxyMiddleware({
+    target: SERVICE.orders,
+    changeOrigin: true,
+    pathRewrite: { '^/api/orders': '/orders' }, 
+});
+app.use('/api/users', proxyWorker);
+app.use('/api/orders', proxyWorker2);
+
+app.listen(port, ()=>{
+    console.log(`proxy is running on ${port}`)
+});
 
 
